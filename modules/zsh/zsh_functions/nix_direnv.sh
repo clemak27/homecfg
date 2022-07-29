7545fbbd4ef0ff2156c0b493ca551dbf92f6688e
clemak27 <clemak27@mailbox.org>
2022-07-29 09:56:57 +0200

    feat(zsh): update direnv script to better handle flake.nix

--- modules/zsh/zsh_functions/nix_direnv.sh
+++ modules/zsh/zsh_functions/nix_direnv.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 UPDATE_GITIGNORE=0
+PERSIST_FLAKE=0
 
 __init_shell() {
   if [ $UPDATE_GITIGNORE -eq 1 ]; then
@@ -12,7 +13,15 @@ __init_shell() {
 
   nix flake new . -t github:nix-community/nix-direnv
   nvim flake.nix
-  git add flake.nix
+
+  if [ $PERSIST_FLAKE -eq 1 ]; then
+    git add flake.nix
+  else
+    # https://discourse.nixos.org/t/can-i-use-flakes-within-a-git-repo-without-committing-flake-nix/18196/5
+    git add --intent-to-add flake.nix
+    git update-index --assume-unchanged flake.nix
+  fi
+
   direnv allow
 }
 
@@ -20,12 +29,17 @@ __remove_shell() {
   rm -rf .direnv .envrc flake.nix flake.lock
 }
 
+# shellcheck disable=SC3033
 nix-direnv() {
   for arg in "$@"
   do
     case $arg in
       -i|--ignore)
         UPDATE_GITIGNORE=1
+        shift
+        ;;
+      -p|--persist)
+        PERSIST_FLAKE=1
         shift
         ;;
       init)
