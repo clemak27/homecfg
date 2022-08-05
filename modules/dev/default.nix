af6ea3213dfe5286cd9799a4afd7f5f28dd329d5
clemak27 <clemak27@mailbox.org>
2022-08-05 06:57:29 +0200

    fest(dev): add gradle-completion plugin

--- modules/dev/default.nix
+++ modules/dev/default.nix
@@ -19,7 +19,15 @@ in
     programs.zsh.oh-my-zsh.plugins = [
       "npm"
       "golang"
+      "gradle-completion"
     ];
+
+    home.file.".oh-my-zsh/custom/plugins/gradle-completion".source =
+      builtins.fetchGit {
+        url = "https://github.com/gradle/gradle-completion";
+        ref = "master";
+        rev = "b042038e3d3b30a6440c121268894234c509ca1c";
+      };
 
     home.file.".npmrc".text = ''
       prefix=~/.local/bin/npm
