be71e4e5e5dcfd8761303d3cf4781e6d4c8b243e
clemak27 <clemak27@mailbox.org>
2022-08-14 14:11:00 +0200

    chore: remove unneeded NixOS module

--- default.nix
+++ default.nix
@@ -12,10 +12,6 @@
     ./modules/zsh
   ];
 
-  options.homecfg.NixOS = {
-    enable = lib.mkEnableOption "Set to true if running on NixOS";
-  };
-
   config = {
     programs.home-manager.enable = true;
     nixpkgs.config.allowUnfree = true;
