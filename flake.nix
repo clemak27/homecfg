e65bcf93f8037c8b79f148da7811227bbb4f47fe
clemak27 <clemak27@mailbox.org>
2022-12-08 15:41:29 +0100

    feat: add nvim-packer to flake

--- flake.nix
+++ flake.nix
@@ -1,9 +1,18 @@
 {
-  inputs = { };
+  inputs = {
+      packer-nvim = { url = "github:wbthomason/packer.nvim"; flake = false; };
+    };
 
   outputs = { self, ... }: {
     nixosModules = {
       homecfg = import ./default.nix;
+      nvim-packer = { config, lib, pkgs, ... }:
+        let
+          packer_dir     = ".local/share/nvim/site/pack/packer/start/";
+        in
+        {
+          home.file."${packer_dir}packer.nvim".source = self.inputs.packer-nvim.outPath;
+       };
     };
   };
 }
