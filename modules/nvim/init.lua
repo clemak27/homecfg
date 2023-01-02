43afc971b20b36e59d73e3dc746a5aed9cfecc24
clemak27 <clemak27@mailbox.org>
2023-01-02 11:38:55 +0100

    chore(nvim): use lua-native autocmd for reopening

--- modules/nvim/init.lua
+++ modules/nvim/init.lua
@@ -93,12 +93,11 @@ vim.api.nvim_create_autocmd({ "BufReadPost" }, {
   pattern = "*",
   group = "reopen_pos",
   callback = function()
-    vim.api.nvim_exec(
-      [[
-    if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif
-    ]] ,
-      false
-    )
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
   end,
 })
 
