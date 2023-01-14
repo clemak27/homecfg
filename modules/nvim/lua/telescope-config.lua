0782e9705d04404a45df5c9c0f26c8df7bf4ec35
clemak27 <clemak27@mailbox.org>
2023-01-14 13:24:58 +0100

    chore(nvim): remove unused config

--- modules/nvim/lua/telescope-config.lua
+++ modules/nvim/lua/telescope-config.lua
@@ -26,11 +26,6 @@ M.load = function()
         case_mode = "smart_case", -- or "ignore_case" or "respect_case"
         -- the default case_mode is "smart_case"
       },
-      ["ui-select"] = {
-        require("telescope.themes").get_dropdown({
-          -- even more opts
-        }),
-      },
     },
   })
 
