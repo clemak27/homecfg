20324c819c3fbbb273bc3a8d4c6c82d4acf55b75
clemak27 <clemak27@mailbox.org>
2023-01-25 17:00:30 +0100

    fix(tools): fix setting fzf theme

--- modules/tools/default.nix
+++ modules/tools/default.nix
@@ -64,7 +64,6 @@ in
         "--border=sharp"
         "--margin=2"
         "--padding=1"
-        # https://github.com/catppuccin/fzf
         "--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8"
         "--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc"
         "--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
