0fbc683a4b430eae4773267362d649b8d6aae174
clemak27 <clemak27@mailbox.org>
2023-01-29 13:08:28 +0100

    feat(nvim): migrate to lazy.nvim

--- modules/nvim/init.lua
+++ modules/nvim/init.lua
@@ -218,4 +218,17 @@ vim_map("ß", "$")
 
 ----------------------------------------- plugins -----------------------------------------
 
-require("packer-nvim").load()
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup("plugins")
