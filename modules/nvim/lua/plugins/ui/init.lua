4d2d5d6929562f091c821cbe0e31c1265481ac82
clemak27 <clemak27@mailbox.org>
2023-01-29 13:38:15 +0100

    feat(nvim): move UI plugins to subdir

---
+++ modules/nvim/lua/plugins/ui/init.lua
@@ -0,0 +1,37 @@
+return {
+  require("plugins.ui.bufferline"),
+  require("plugins.ui.catppuccin"),
+  require("plugins.ui.gitsigns"),
+  require("plugins.ui.lualine"),
+  require("plugins.ui.noice"),
+  require("plugins.ui.nvim-tree"),
+  require("plugins.ui.telescope"),
+  require("plugins.ui.treesitter"),
+
+  "kyazdani42/nvim-web-devicons",
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      require("colorizer").setup()
+    end,
+  },
+  {
+    "petertriho/nvim-scrollbar",
+    config = function()
+      require("scrollbar").setup({
+        handle = {
+          color = "#45475A",
+        },
+      })
+    end,
+  },
+  {
+    "RRethy/vim-illuminate",
+    config = function()
+      require("illuminate").configure({
+        delay = 1000,
+        filetypes_denylist = { "NvimTree" },
+      })
+    end,
+  },
+}
