ef2cfc77a3e08716f9e88e325e7d3ebad3973087
clemak27 <clemak27@mailbox.org>
2023-01-29 13:08:43 +0100

    feat(nvim): remove packer module

--- flake.nix
+++ flake.nix
@@ -1,18 +1,9 @@
 {
-  inputs = {
-      packer-nvim = { url = "github:wbthomason/packer.nvim"; flake = false; };
-    };
+  inputs = { };
 
   outputs = { self, ... }: {
     nixosModules = {
       homecfg = import ./default.nix;
-      nvim-packer = { config, lib, pkgs, ... }:
-        let
-          packer_dir     = ".local/share/nvim/site/pack/packer/start/";
-        in
-        {
-          home.file."${packer_dir}packer.nvim".source = self.inputs.packer-nvim.outPath;
-       };
     };
   };
 }
