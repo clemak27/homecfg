e5a17b17f0be0cbd61cbe80be167f119dbf06b85
clemak27 <clemak27@mailbox.org>
2023-05-08 09:57:02 +0200

    fix(nvim): update lspconfig icons

--- modules/nvim/lua/plugins/lsp/nvim-lspconfig.lua
+++ modules/nvim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -210,7 +210,7 @@ return {
       setup_servers()
 
       -- customize signs
-      local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+      local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
       for type, icon in pairs(signs) do
         local hl = "DiagnosticSign" .. type
         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
