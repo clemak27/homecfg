176c9f583770bc28c7bc356481e481a5c2b340d2
clemak27 <clemak27@mailbox.org>
2023-05-14 12:15:55 +0200

    fix(nvim): load eslint only when available

--- modules/nvim/lua/plugins/lsp/null-ls.lua
+++ modules/nvim/lua/plugins/lsp/null-ls.lua
@@ -3,35 +3,61 @@ return {
   {
     "jose-elias-alvarez/null-ls.nvim",
     config = function()
+      local function file_exists(name)
+        local f = io.open(name, "r")
+        if f ~= nil then
+          io.close(f)
+          return true
+        else
+          return false
+        end
+      end
+
       local null_ls = require("null-ls")
       local code_actions = null_ls.builtins.code_actions
       local diagnostics = null_ls.builtins.diagnostics
       local formatting = null_ls.builtins.formatting
 
-      require("null-ls").setup({
-        sources = {
+      local nullLsSources = {
+        diagnostics.markdownlint.with({
+          diagnostics_format = "[#{c}] #{m} (#{s})",
+        }),
+
+        code_actions.shellcheck,
+
+        formatting.shfmt.with({
+          extra_args = { "-i", "2", "-sr", "-ci" },
+        }),
+        formatting.stylua,
+        formatting.goimports,
+        diagnostics.yamllint,
+        diagnostics.hadolint,
+        diagnostics.golangci_lint,
+      }
+
+      if file_exists(vim.fn.getcwd() .. "/node_modules/.bin/eslint") then
+        table.insert(
+          nullLsSources,
           code_actions.eslint.with({
             prefer_local = "node_modules/.bin",
-          }),
+          })
+        )
+        table.insert(
+          nullLsSources,
           diagnostics.eslint.with({
             prefer_local = "node_modules/.bin",
-          }),
+          })
+        )
+        table.insert(
+          nullLsSources,
           formatting.eslint.with({
             prefer_local = "node_modules/.bin",
-          }),
-          diagnostics.markdownlint.with({
-            diagnostics_format = "[#{c}] #{m} (#{s})",
-          }),
-          code_actions.shellcheck,
-          formatting.shfmt.with({
-            extra_args = { "-i", "2", "-sr", "-ci" },
-          }),
-          formatting.stylua,
-          formatting.goimports,
-          diagnostics.yamllint,
-          diagnostics.hadolint,
-          diagnostics.golangci_lint,
-        },
+          })
+        )
+      end
+
+      require("null-ls").setup({
+        sources = nullLsSources,
       })
 
       vim.api.nvim_create_user_command("NullLsToggle", function()
