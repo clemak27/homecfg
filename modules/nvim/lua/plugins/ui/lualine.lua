ff350a837dd41ec7cf75b2a5790525ebffcac591
clemak27 <clemak27@mailbox.org>
2023-05-27 21:36:54 +0200

    feat(nvim): update lualine config

--- modules/nvim/lua/plugins/ui/lualine.lua
+++ modules/nvim/lua/plugins/ui/lualine.lua
@@ -3,32 +3,55 @@
 return {
   "nvim-lualine/lualine.nvim",
   config = function()
+    local C = require("catppuccin.palettes").get_palette("mocha")
     require("lualine").setup({
       options = {
         theme = "catppuccin",
         section_separators = "",
-        component_separators = "|",
+        component_separators = "",
       },
       sections = {
         lualine_a = {},
-        lualine_b = { "mode" },
+        lualine_b = {},
         lualine_c = {
-          "filename",
+          {
+            "mode",
+            color = function(section)
+              if vim.fn.mode() == "n" then
+                return { fg = C.blue, gui = "bold" }
+              elseif vim.fn.mode() == "i" then
+                return { fg = C.teal, gui = "bold" }
+              elseif vim.fn.mode() == "t" then
+                return { fg = C.teal, gui = "bold" }
+              elseif vim.fn.mode() == "c" then
+                return { fg = C.peach, gui = "bold" }
+              elseif vim.fn.mode() == "v" then
+                return { fg = C.mauve, gui = "bold" }
+              elseif vim.fn.mode() == "r" then
+                return { fg = C.red, gui = "bold" }
+              end
+            end,
+          },
           "filetype",
+          {
+            "filename",
+            separator = "|",
+          },
         },
         lualine_x = {
           {
-            "fileformat",
-            icon = false,
           },
-          "encoding",
-          "progress",
+          "searchcount",
           "location",
+          "diff",
+          { "branch", icon = "", color = { fg = C.green, gui = "bold" } },
         },
-        lualine_y = { { "branch", icon = "" } },
+        lualine_y = {},
         lualine_z = {},
       },
       tabline = {},
+      winbar = {},
+      inactive_winbar = {},
       extensions = {},
     })
   end,
