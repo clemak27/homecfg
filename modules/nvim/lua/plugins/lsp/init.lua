1eed488dbcfc930174f7538212ca41ebdbf284fb
clemak27 <clemak27@mailbox.org>
2023-07-19 08:33:13 +0200

    feat(nvim): remove null-ls

--- modules/nvim/lua/plugins/lsp/init.lua
+++ modules/nvim/lua/plugins/lsp/init.lua
@@ -1,6 +1,5 @@
 return {
   require("plugins.lsp.mason"),
-  require("plugins.lsp.null-ls"),
   require("plugins.lsp.nvim-dap"),
   require("plugins.lsp.nvim-lspconfig"),
   require("plugins.lsp.trouble"),
