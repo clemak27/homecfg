ae9a0a11d6b2c707a5e78ffbd04c58d63dce9dd3
clemak27 <clemak27@mailbox.org>
2023-07-28 12:42:13 +0200

    fix(zellij): split hx specific code from default.nix

--- modules/zellij/default.nix
+++ modules/zellij/default.nix
@@ -13,26 +13,12 @@ let
       zellij action new-tab --cwd $path --name $pname --layout dev
     fi
   '';
-
-  sidetree = pkgs.rustPlatform.buildRustPackage rec {
-    pname = "sidetree";
-    version = "v0.8.2";
-
-    src = pkgs.fetchFromGitHub {
-      owner = "topisani";
-      repo = pname;
-      rev = version;
-      hash = "sha256-3Pq1Ta3BgE68Z9de9b11Im+Zrm1vZxhNHf1rWDAT34g";
-    };
-
-    cargoSha256 = "sha256-4W1M3Y4lckQU5t7kaYJqOwkF6nFlTYWUBfC2LVUkKI8=";
-  };
-
-  zellijHelixSideTree = pkgs.writeShellScriptBin "zhst" ''
-    zellij run -c -f -d left -- sidetree
-  '';
 in
 {
+  imports = [
+    ./hx.nix
+  ];
+
   options.homecfg.zellij = {
     enable = lib.mkEnableOption "Manage zellij with home-manager";
   };
@@ -45,8 +31,6 @@ in
     };
 
     home.packages = [
-      sidetree
-      zellijHelixSideTree
       zellijLazygit
       zellijOpenProject
     ];
@@ -54,19 +38,6 @@ in
     xdg.configFile = {
       "zellij/config.kdl".source = ./config.kdl;
       "zellij/layouts/custom.kdl".source = ./custom.kdl;
-      "zellij/layouts/dev.kdl".source = ./dev.kdl;
-      "sidetree/sidetreerc".text = ''
-        set show_hidden true
-        set quit_on_open false
-        set open_cmd 'zellij action move-focus right && zellij action write-chars ":open $sidetree_entry"'
-
-        set file_icons false
-        set icon_style white
-        set dir_name_style lightblue+b
-        set file_name_style reset
-        set highlight_style +r
-        set link_style cyan+b
-      '';
     };
   };
 }
---
+++ modules/zellij/hx.nix
@@ -0,0 +1,47 @@
+{ config, lib, pkgs, ... }:
+let
+  cfg = config.homecfg;
+  sidetree = pkgs.rustPlatform.buildRustPackage rec {
+    pname = "sidetree";
+    version = "v0.8.2";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "topisani";
+      repo = pname;
+      rev = version;
+      hash = "sha256-3Pq1Ta3BgE68Z9de9b11Im+Zrm1vZxhNHf1rWDAT34g";
+    };
+
+    cargoSha256 = "sha256-4W1M3Y4lckQU5t7kaYJqOwkF6nFlTYWUBfC2LVUkKI8=";
+  };
+
+  zellijHelixSideTree = pkgs.writeShellScriptBin "zhst" ''
+    zellij run -c -f -d left -- sidetree
+  '';
+in
+{
+  config = lib.mkIf (cfg.helix.enable) {
+
+    home.packages = [
+      sidetree
+      zellijHelixSideTree
+    ];
+
+    xdg.configFile = {
+      "zellij/layouts/dev.kdl".source = ./devHx.kdl;
+      "sidetree/sidetreerc".text = ''
+        set show_hidden true
+        set quit_on_open false
+        set open_cmd 'zellij action move-focus right && zellij action write-chars ":open $sidetree_entry"'
+
+        set file_icons false
+        set icon_style white
+        set dir_name_style lightblue+b
+        set file_name_style reset
+        set highlight_style +r
+        set link_style cyan+b
+      '';
+    };
+
+  };
+}
