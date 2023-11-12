8ca587c9db49719db80deb4a17187124a45eb9e9
clemak27 <clemak27@mailbox.org>
2023-11-12 09:35:33 +0100

    fix(nvim): lower autosession loglevel

--- modules/nvim/lua/plugins.lua
+++ modules/nvim/lua/plugins.lua
@@ -62,8 +62,9 @@ return {
   {
     "rmagatti/auto-session",
     config = function()
+      vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
       require("auto-session").setup({
-        log_level = "info",
+        log_level = "error",
         auto_session_suppress_dirs = { "~/", "~/Projects" },
       })
     end,
