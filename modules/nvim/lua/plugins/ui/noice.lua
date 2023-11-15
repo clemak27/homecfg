555204c1215b8246b3350794e781b0cb2869ab34
clemak27 <clemak27@mailbox.org>
2023-11-15 08:26:57 +0100

    feat(nvim): filter some default messages from appearing in noice

--- modules/nvim/lua/plugins/ui/noice.lua
+++ modules/nvim/lua/plugins/ui/noice.lua
@@ -14,6 +14,8 @@ return {
     },
   },
   config = function()
+    vim.opt.shortmess:append("IWs")
+
     require("noice").setup({
       messages = {
         view_search = false,
@@ -29,6 +31,24 @@ return {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
           ["vim.lsp.util.stylize_markdown"] = true,
           ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "; after #%d+" },
+              { find = "; before #%d+" },
+              { find = "%d fewer lines" },
+              { find = "%d more lines" },
+              { find = "%d lines changed" },
+              { find = "%d lines yanked" },
+              { find = "Replaced %d line with %d lines" },
+            },
+          },
+          opts = { skip = true },
         },
       },
       presets = {
