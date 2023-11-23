765761eb8b6d2d5410dbd96b983bfd84689896ee
clemak27 <clemak27@mailbox.org>
2023-11-23 16:34:07 +0100

    feat(nvim): add kdl grammar

--- modules/nvim/lua/plugins/ui/treesitter.lua
+++ modules/nvim/lua/plugins/ui/treesitter.lua
@@ -13,6 +13,14 @@ return {
       },
       filetype = "hurl",
     }
+    parser_config.kdl = {
+      install_info = {
+        url = "https://github.com/amaanq/tree-sitter-kdl",
+        files = { "src/parser.c", "src/scanner.c" },
+        branch = "master",
+      },
+      filetype = "kdl",
+    }
 
     require("nvim-treesitter.configs").setup({
       ensure_installed = "all",
