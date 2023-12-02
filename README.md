fa2b50b33be305fe9f0379e05d08dc8413d58cda
clemak27 <clemak27@mailbox.org>
2023-12-02 09:24:00 +0100

    feat!: migrate to home-manager module

--- README.md
+++ README.md
@@ -7,46 +7,49 @@ be enabled/disabled as needed.
 
 ## Usage
 
-The flake provides a NixOS module, which can be imported in an existing config.
+The flake provides a home-manager module, which can be imported in an existing
+config.
 
-1. Add this input to you `flake.nix`:
+Add this input to you `flake.nix`:
 
-   ```nix
-   homecfg = {
-     url = "github:clemak27/homecfg";
-   };
-   ```
+```nix
+homecfg = {
+  url = "github:clemak27/homecfg";
+};
+```
 
-2. Add it as module to your `homeManagerConfiguration`:
+Add it as module to your `homeManagerConfiguration`:
 
-   ```nix
-   modules = [
-     homecfg.nixosModules.homecfg
-     ...
-   ];
-   ```
+```nix
+modules = [
+  homecfg.hmModules.homecfg
+  ...
+];
+```
 
-3. For local development/changes to `homecfg`, use the absolute path to the
-   checked out git repo when switching config, e.g.
-   `home-manager switch --flake . --impure --override-input homecfg 'path:<path-to-homecfg>'"`
+For local development/changes to `homecfg`, use the absolute path to the checked
+out git repo when switching config, e.g.
+`home-manager switch --flake . --impure --override-input homecfg 'path:<path-to-homecfg>'"`
 
 ## Notes
 
-### Updating and nixpkgs
+### Updating and `nixpkgs`
 
-Managing the `nixpkgs` config you use is out of scope for this project and is
-part of your setup. This is just a module.
-
-Note that if you want to update just `homecfg`, instead of your whole flake, use
-`nix flake lock --update-input homecfg`.
-
-### Aliases
-
-some aliases that are useful:
+The versions of `nixpkgs` included here might not be up-to-date. It is suggested
+to pin the versions to whatever you use in your flake like this:
 
 ```nix
-{ name = "hms"; value = "home-manager switch --flake . --impure"; }
-{ name = "hmsl"; value = "home-manager switch --flake . --impure --override-input homecfg 'path:<path-to-homecfg>'"; }
+nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+home-manager = {
+  url = "github:nix-community/home-manager/master";
+  inputs.nixpkgs.follows = "nixpkgs";
+};
+homecfg = {
+  url = "github:clemak27/homecfg";
+  inputs.nixpkgs.follows = "nixpkgs";
+  inputs.home-manager.follows = "home-manager";
+};
 ```
 
 ### non-NixOS
--- flake.nix
+++ flake.nix
@@ -1,9 +1,89 @@
 {
-  inputs = { };
-
-  outputs = { self, ... }: {
-    nixosModules = {
-      homecfg = import ./default.nix;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs = { self, nixpkgs, home-manager, pre-commit-hooks }:
+    let
+      supportedSystems = [
+        "aarch64-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system:
+        import nixpkgs { inherit system; });
+    in
+    {
+      hmModules.homecfg = { ... }: {
+        imports = [
+          ./modules/dev
+          ./modules/fun
+          ./modules/git
+          ./modules/k8s
+          ./modules/nvim
+          ./modules/todo
+          ./modules/tools
+          ./modules/zsh
+          ./modules/zellij
+        ];
+
+        config = {
+          programs.home-manager.enable = true;
+          nixpkgs.config.allowUnfree = true;
+        };
+      };
+
+      checks = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              editorconfig-checker.enable = true;
+              nixpkgs-fmt.enable = true;
+
+              conventional_commits = {
+                enable = true;
+                name = "commit message";
+                description = ''
+                  Check whether the current commit message follows commiting rules.
+                '';
+                entry =
+                  let
+                    script = pkgs.writeShellScript "pre-commit-conventional" ''
+                      commit_msg=$(cat "$1")
+                      regex="^(fixup! )?(build|chore|ci|docs|feat|feat!|fix|perf|refactor|revert|style|test)(\(.*\))?: .*$"
+                      error="Commit message does not conform to regex: $regex"
+                      [[ "$commit_msg" =~ $regex ]] && exit 0 || (echo $error && exit 1)
+                    '';
+                  in
+                  toString
+                    script;
+                stages = [ "commit-msg" ];
+              };
+            };
+          };
+        });
+
+      devShells = forAllSystems
+        (system: {
+          default = nixpkgsFor.${system}.mkShell
+            {
+              shellHook = (self.checks.${system}.pre-commit-check.shellHook);
+            };
+        });
+    };
 }
+
