9a5679593f7da82148264ae6d017f6e18dd84eef
clemak27 <clemak27@mailbox.org>
2024-07-14 12:48:12 +0200

    fix(nvim): remove duplicate mapping

--- modules/nvim/lua/plugins/ui/gitsigns.lua
+++ modules/nvim/lua/plugins/ui/gitsigns.lua
@@ -29,7 +29,6 @@ return {
         map("n", "<leader>tb", "<cmd>Gitsigns toggle_current_line_blame<CR>")
         map("n", "<leader>hd", "<cmd>Gitsigns diffthis<CR>")
         map("n", "<leader>hD", '<cmd>lua require"gitsigns".diffthis("~")<CR>')
-        map("n", "<leader>td", "<cmd>Gitsigns toggle_deleted<CR>")
 
         -- Text object
         map("o", "ih", ":<C-U>Gitsigns select_hunk<CR>")
