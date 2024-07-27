94d7dd5f0225650830ea556d5bd110a70a82e9f6
clemak27 <clemak27@mailbox.org>
2024-07-27 14:35:55 +0200

    feat(nvim): replace peek with markview

--- modules/nvim/lua/plugins/filetypes.lua
+++ modules/nvim/lua/plugins/filetypes.lua
@@ -27,31 +27,11 @@ return {
     end,
   },
   {
-    "preservim/vim-markdown",
+    "OXY2DEV/markview.nvim",
     ft = { "markdown" },
-    config = function()
-      vim.o.conceallevel = 2
-      vim.api.nvim_exec2(
-        [[
-        let g:vim_markdown_folding_disabled = 1
-        let g:vim_markdown_emphasis_multiline = 0
-        let g:vim_markdown_conceal_code_blocks = 0
-        let g:vim_markdown_new_list_item_indent = 2
-        ]],
-        { output = false }
-      )
-    end,
-  },
-  {
-    "toppair/peek.nvim",
-    event = { "VeryLazy" },
-    build = "deno task --quiet build:fast",
-    config = function()
-      require("peek").setup({
-        app = "browser",
-      })
-      vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
-      vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
-    end,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
   },
 }
