b2496142064c4defc029295233422820c64381e9
clemak27 <clemak27@mailbox.org>
2024-07-28 13:21:11 +0200

    feat(nvim): add autolist.nvim

--- modules/nvim/lua/plugins/filetypes.lua
+++ modules/nvim/lua/plugins/filetypes.lua
@@ -27,6 +27,24 @@ return {
     end,
   },
   {
+    "gaoDean/autolist.nvim",
+    ft = { "markdown" },
+    config = function()
+      require("autolist").setup()
+
+      vim.keymap.set("i", "<CR>", "<CR><cmd>AutolistNewBullet<cr>")
+      vim.keymap.set("n", "o", "o<cmd>AutolistNewBullet<cr>")
+      vim.keymap.set("n", "O", "O<cmd>AutolistNewBulletBefore<cr>")
+      vim.keymap.set("n", "<CR>", "<cmd>AutolistToggleCheckbox<cr><CR>")
+      vim.keymap.set("n", "<C-r>", "<cmd>AutolistRecalculate<cr>")
+      -- functions to recalculate list on edit
+      vim.keymap.set("n", ">>", ">><cmd>AutolistRecalculate<cr>")
+      vim.keymap.set("n", "<<", "<<<cmd>AutolistRecalculate<cr>")
+      vim.keymap.set("n", "dd", "dd<cmd>AutolistRecalculate<cr>")
+      vim.keymap.set("v", "d", "d<cmd>AutolistRecalculate<cr>")
+    end,
+  },
+  {
     "OXY2DEV/markview.nvim",
     ft = { "markdown" },
     dependencies = {
